be008f0339cb4a162018d454a0932cb2c5abdc7d
zyr3x <izzzum@gmail.com>
2013-10-17 12:43:41 +0300

    Add color picker

--- src/com/android/settings/romextras/ROMExtras.java
+++ src/com/android/settings/romextras/ROMExtras.java
@@ -27,13 +27,17 @@ import android.provider.Settings;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
 
 public class ROMExtras extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String EXTRA_CRT = "extra_crt";
     private static final String EXTRA_BATTERY_TEXT = "extra_battery_text";
     private static final String MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
+    private static final String EXTRA_STATUSBAR_COLOR = "extra_statusbar_color";
 
+    private ColorPickerPreference mStatusBarColor;
     private CheckBoxPreference mToggleCrt;
     private CheckBoxPreference mToggleBatteryText;
     private ListPreference mMSOB;
@@ -44,12 +48,13 @@ public class ROMExtras extends SettingsPreferenceFragment implements OnPreferenc
         addPreferencesFromResource(R.xml.romextras_settings);
         initializeAllPreferences();
     }
+
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         if (mToggleCrt == preference) {
-            Settings.System.putInt(getContentResolver(),Settings.System.SYSTEM_POWER_ENABLE_CRT_OFF, mToggleCrt.isChecked() ? 1 : 0 );
+            Settings.System.putInt(getContentResolver(), Settings.System.SYSTEM_POWER_ENABLE_CRT_OFF, mToggleCrt.isChecked() ? 1 : 0);
             return true;
         } else if (mToggleBatteryText == preference) {
-            Settings.System.putInt(getContentResolver(),Settings.System.STATUSBAR_BATTERY_ICON, mToggleBatteryText.isChecked() ? 1 : 0 );
+            Settings.System.putInt(getContentResolver(), Settings.System.STATUSBAR_BATTERY_ICON, mToggleBatteryText.isChecked() ? 1 : 0);
             return true;
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
@@ -59,20 +64,32 @@ public class ROMExtras extends SettingsPreferenceFragment implements OnPreferenc
         if (preference == mMSOB) {
             writeMSOBOptions(newValue);
             return true;
+        } else if (preference == mStatusBarColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUSBAR_COLOR, intHex);
+            return true;
         }
+
         return true;
     }
 
     private void initializeAllPreferences() {
         mToggleCrt = (CheckBoxPreference) findPreference(EXTRA_CRT);
-        mToggleCrt.setChecked(  (Settings.System.getInt(getContentResolver(),Settings.System.SYSTEM_POWER_ENABLE_CRT_OFF, 0 ) == 1) );
+        mToggleCrt.setChecked((Settings.System.getInt(getContentResolver(), Settings.System.SYSTEM_POWER_ENABLE_CRT_OFF, 0) == 1));
 
-        mToggleBatteryText = (CheckBoxPreference) findPreference(EXTRA_BATTERY_TEXT );
-        mToggleBatteryText.setChecked(  (Settings.System.getInt(getContentResolver(),Settings.System.STATUSBAR_BATTERY_ICON, 1 ) == 1) );
+        mToggleBatteryText = (CheckBoxPreference) findPreference(EXTRA_BATTERY_TEXT);
+        mToggleBatteryText.setChecked((Settings.System.getInt(getContentResolver(), Settings.System.STATUSBAR_BATTERY_ICON, 1) == 1));
 
         mMSOB = (ListPreference) findPreference(MEDIA_SCANNER_ON_BOOT);
         updateMSOBOptions();
         mMSOB.setOnPreferenceChangeListener(this);
+
+        mStatusBarColor = (ColorPickerPreference) findPreference(EXTRA_STATUSBAR_COLOR);
+        mStatusBarColor.setOnPreferenceChangeListener(this);
     }
 
 
